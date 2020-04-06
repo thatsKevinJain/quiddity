d380961cc23607c2c09d1d2dca91987fd28d1f9a
Kevin <thatskevinjain@gmail.com>
2020-04-06 11:03:39 +0530

    Removed module require-all, added recursive logic for require-all

--- api/commands/index.js
+++ api/commands/index.js
@@ -2,4 +2,19 @@
 	Get all the avaible DB Commands
 */
 
-module.exports = require('require-all')(__dirname + '/')
+var normalizedPath = require("path").join(__dirname, "/")
+
+const commands = {}
+
+require("fs")
+.readdirSync(normalizedPath)
+.forEach(function(file) {
+
+	// Remove the .js extension from filename //
+	const command = file.split(".js").join("").trim().toString()
+
+	// Require the file and append to commands //
+	commands[command] = require("./" + file)
+})
+
+module.exports = commands
