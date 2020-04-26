994b4223a79521fc0907f832a05904873a635794
Kevin Jain <thatskevinjain@gmail.com>
2020-04-26 18:53:30 +0530

    Added explanation comments to utils

--- api/services/utils.js
+++ api/services/utils.js
@@ -26,7 +26,26 @@ module.exports = {
 			resolve()
 		})
 	},
-
+	
+	/**
+		To filter the results based on user passed query,
+		we need to format the query params so that they match the format in DB.
+		Example (If the query looks something like below) -
+		Input -
+		{
+			foo: "bar",
+			count: {$gt: 10}
+		}
+		The function will map the input and return the below - 
+		Output - 
+		{
+			"payload.foo": "bar",
+			"payload.count": {$gt: 10}
+		}
+		This is done because user messages are stored in "payload" object.
+		
+		We will safely ignore all operators ($or, $gt, $nor, etc) to safeguard their usage.
+	*/
 	getQueryParams: function(where) {
 		// Define an empty object //
 		let result = {}
